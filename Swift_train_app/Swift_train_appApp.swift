9bd9b3f6818007750efc65012ffd9218d1866dcd
Barisdemirci3 <baris19052003@gmail.com>
2024-04-05 00:58:31 +0300

    Initial Commit

---
+++ Swift_train_app/Swift_train_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_train_appApp.swift
+//  Swift_train_app
+//
+//  Created by Barış Demirci on 5.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_train_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
