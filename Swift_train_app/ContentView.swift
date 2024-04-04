b3690495c7e148345271c8d915026e5ff84c9a46
Barisdemirci3 <baris19052003@gmail.com>
2024-04-05 01:00:24 +0300

    File added

--- Swift_train_app/ContentView.swift
+++ Swift_train_app/ContentView.swift
@@ -1,23 +1,67 @@
 //
 //  ContentView.swift
-//  Swift_train_app
+//  Deneme-proje
 //
-//  Created by Barış Demirci on 5.04.2024.
+//  Created by Barış Demirci on 4.04.2024.
 //
 
 import SwiftUI
 
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var username: String = ""
+        @State private var password: String = ""
+        @State private var isLoggedIn: Bool = false
+    @State private var showingAlert: Bool = false
+        
+        var body: some View {
+            ZStack{
+                Rectangle().fill(Color.blue).frame(width: 500).rotationEffect(.degrees(11))
+                Text("Kayıt Ol").font(.system(size: 70, design: .rounded).bold()).foregroundColor(.white)
+            }
+            VStack {
+                Spacer()
+                VStack(spacing: 20){
+                    TextField("Kullanıcı Adı", text: $username)
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(8)
+                        .padding(.bottom, 20)
+                    
+                    SecureField("Şifre", text: $password)
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(8)
+                        .padding(.bottom, 20)
+                    
+                    Button(action: {
+                        // Burada giriş işlemleri yapılabilir
+                        // Örnek olarak, kullanıcı adı ve şifreyi kontrol edip isLoggedIn değerini güncelleyebilirsiniz
+                        isLoggedIn = (username == "kullanici" && password == "sifre")
+                    }) {
+                        Text("Giriş Yap")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                    }
+                }
+                
+                if isLoggedIn {
+                    Text("Giriş Başarılı!")
+                        .foregroundColor(.green)
+                        .padding(.top, 20)
+                }
+                
+               
+            }
+            .padding()
+            Text("Henüz bir hesabın yok mu?")
+            
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
